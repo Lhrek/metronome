aa23b9f15baa8e075c3d105a8df74a4e8bf93df0
Zhijie Gan <herujiege@gmail.com>
2024-09-05 17:00:34 +0800

    节拍器 init

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow } = require('electron');
 
-const createWindow = () => {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true,
+      contextIsolation: false
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
--- renderer.js
+++ renderer.js
@@ -1,2 +1,29 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`
+const audioContext = new AudioContext();
+let interval = null;
+const bpmSlider = document.getElementById('bpm-slider');
+const bpmDisplay = document.getElementById('bpm-display');
+
+bpmSlider.oninput = function() {
+  bpmDisplay.textContent = `${this.value} BPM`;
+  if (interval) {
+    stopMetronome();
+    startMetronome();
+  }
+};
+
+function startMetronome() {
+  const bpm = parseInt(bpmSlider.value);
+  const intervalTime = 60000 / bpm;
+  interval = setInterval(() => {
+    const oscillator = audioContext.createOscillator();
+    oscillator.connect(audioContext.destination);
+    oscillator.frequency.value = 440; // A4 音高
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + 0.1); // 持续时间为 0.1 秒
+  }, intervalTime);
+}
+
+function stopMetronome() {
+  clearInterval(interval);
+  interval = null;
+}
